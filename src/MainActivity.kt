97e62520e4e2ff4b0c007e376c2b914280577167
kotfind <kotfind@yandex.ru>
2024-10-06 10:49:00 +0300

    Usable

--- src/MainActivity.kt
+++ src/MainActivity.kt
@@ -4,12 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.kotfind.android_demo.ui.theme.MyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { Text("Hello, World") }
+        setContent {
+            MyTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                    ) { Text("Hello, World") }
+                }
+            }
+        }
     }
 }
